4a71fc4d8c8122d6644ffea0579d031c0cf90335
Kevin Ngo <ngoke@mozilla.com>
2014-04-28 03:33:14 -0700

    Move from side to side

--- media/js/adelheid.js
+++ media/js/adelheid.js
@@ -1,3 +1,11 @@
+window.onscroll = function() {
+    window.scrollTo(0, 0);
+};
+
 $(document).ready(function() {
     $('.chapter.active .image-reel').addClass('ripcord');
+
+    $('.image-strip, .image-reel').on('focus', function() {
+        $(this)[0].blur();
+    });
 });
