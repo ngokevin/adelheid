b78f37a19dcfc8df726cefd3c475082afc6b6829
Kevin Ngo <ngoke@mozilla.com>
2014-05-01 20:32:48 -0700

    Moments.

--- media/js/adelheid.js
+++ media/js/adelheid.js
@@ -9,7 +9,8 @@ $(document).ready(function() {
     var active_chapter_num = 0;
     var num_chapters = $chapters.length;
     var chapter_width = $(window).width();
-    var adelheid_transition_ms = parseInt($adelheid.css('transition-duration'), 10) * 1000;
+    // Time it takes to change a chapter.
+    var adelheid_ms = parseInt($adelheid.css('transition-duration'), 10) * 1000;
 
     /* ==== */
     /* Init */
@@ -33,9 +34,16 @@ $(document).ready(function() {
     /* Changing chapters */
     /* ================= */
     function go_to_chapter(old_chapter, chapter) {
+        if (chapter == old_chapter) {
+            return;
+        }
+
         $get_chapter(old_chapter).removeClass('active');
-        // Parallax.
-        $get_chapter(chapter).addClass('active');
+        setTimeout(function() {
+            // Parallax logic.
+            $get_chapter(chapter).addClass('active');
+            // Delay the transition until we reach second-to-last chapter.
+        }, adelheid_ms - (adelheid_ms / Math.abs(chapter - old_chapter)));
 
         // Translate adelheid.
         var translateX = -1 * chapter * chapter_width;
@@ -45,7 +53,7 @@ $(document).ready(function() {
         // Pause animation.
         setTimeout(function() {
             $get_chapter(old_chapter).find('.image-reel').removeClass('running');
-        }, adelheid_transition_ms);
+        }, adelheid_ms);
 
         // Run new animation.
         $active_chapter().find('.image-reel').addClass('running');
@@ -61,6 +69,22 @@ $(document).ready(function() {
         if (active_chapter_num < num_chapters - 1) {
             go_to_chapter(active_chapter_num, ++active_chapter_num);
         }
+    });
+
+    $('.bubble').on('click', function() {
+        var old_chapter = active_chapter_num;
+        active_chapter_num = $('.bubble').index(this);
+        go_to_chapter(old_chapter, active_chapter_num);
+    });
+
+    /* ====== */
+    /* Moment */
+    /* ====== */
+    $('.moment-link').on('click', function() {
+        var $this = $(this);
+        var slug = $this.data('slug');
+        var $chapter = $this.closest('.chapter-body');
+        $chapter.find('.moment[data-slug="' + slug + '"').show();
     });
 
     /* ========== */
