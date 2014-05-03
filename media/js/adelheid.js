75c3e8272baa60725f90e32540ce5190fb00f7a7
Kevin Ngo <ngoke@mozilla.com>
2014-05-03 11:41:05 -0700

    Bubbly.

--- media/js/adelheid.js
+++ media/js/adelheid.js
@@ -3,6 +3,7 @@ window.onscroll = function() {
 };
 
 $(document).ready(function() {
+    var $body = $(document.body);
     var $adelheid = $('.adelheid');
     var $chapters = $('.chapter');
 
@@ -57,6 +58,9 @@ $(document).ready(function() {
 
         // Run new animation.
         $active_chapter().find('.image-reel').addClass('running');
+
+        $('.bubble').eq(chapter).addClass('active');
+        $('.bubble').eq(chapter + 1).addClass('active');
     }
 
     $('.prev').on('click', function() {
@@ -71,7 +75,7 @@ $(document).ready(function() {
         }
     });
 
-    $('.bubble').on('click', function() {
+    $body.on('click', 'nav .bubble.active', function() {
         var old_chapter = active_chapter_num;
         active_chapter_num = $('.bubble').index(this);
         go_to_chapter(old_chapter, active_chapter_num);
@@ -96,7 +100,7 @@ $(document).ready(function() {
         $chapter.find('.flip-container').removeClass('flipped');
         setTimeout(function() {
             $chapter.find('.moment').removeClass('active');
-        }, 2000);
+        }, 500);
     });
 
     /* ========== */
